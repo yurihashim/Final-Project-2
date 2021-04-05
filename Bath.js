949e8ea664f713b0237c28ca04ade536a2f3bc3c
yurihashim <yurihashim@icloud.com>
2021-04-05 15:05:53 -0700

    first commit

---
+++ Bath.js
@@ -0,0 +1,214 @@
+var cart = {
+    // (A) PROPERTIES
+    hPdt : null, // HTML products list
+    hItems : null, // HTML current cart
+    items : {}, // Current items in cart
+  
+    // (B) LOCALSTORAGE CART
+    // (B1) SAVE CURRENT CART INTO LOCALSTORAGE
+    save : function () {
+      localStorage.setItem("cart", JSON.stringify(cart.items));
+    },
+  
+    // (B2) LOAD CART FROM LOCALSTORAGE
+    load : function () {
+      cart.items = localStorage.getItem("cart");
+      if (cart.items == null) { cart.items = {}; }
+      else { cart.items = JSON.parse(cart.items); }
+    },
+  
+    // (B3) EMPTY ENTIRE CART
+    nuke : function () {
+      if (confirm("Empty cart?")) {
+        cart.items = {};
+        localStorage.removeItem("cart");
+        cart.list();
+      }
+    },
+  
+    // (C) INITIALIZE
+    init : function () {
+      // (C1) GET HTML ELEMENTS
+      cart.hPdt = document.getElementById("cart-products");
+      cart.hItems = document.getElementById("cart-items");
+  
+      // (C2) DRAW PRODUCTS LIST
+      cart.hPdt.innerHTML = "";
+      let BathP, item, part;
+      for (let id in Bathproducts) {
+        // WRAPPER
+        BathP= Bathproducts[id];
+        item = document.createElement("div");
+        item.className = "p-item";
+        cart.hPdt.appendChild(item);
+  
+        // PRODUCT IMAGE
+        part = document.createElement("img");
+        part.src = "images/" + BathP.img;
+        part.className = "p-img";
+        item.appendChild(part);
+  
+        // PRODUCT NAME
+        part = document.createElement("div");
+        part.innerHTML = BathP.name;
+        part.className = "p-name";
+        item.appendChild(part);
+  
+        // PRODUCT DESCRIPTION
+        part = document.createElement("div");
+        part.innerHTML = BathP.desc;
+        part.className = "p-desc";
+        item.appendChild(part);
+  
+        // PRODUCT PRICE
+        part = document.createElement("div");
+        part.innerHTML = "$" + BathP.price;
+        part.className = "p-price";
+        item.appendChild(part);
+  
+        // ADD TO CART
+        part = document.createElement("input");
+        part.type = "button";
+        part.value = "Add to Cart";
+        part.className = "cart p-add";
+        part.onclick = cart.add;
+        part.dataset.id = id;
+        item.appendChild(part);
+      }
+  
+      // (C3) LOAD CART FROM PREVIOUS SESSION
+      cart.load();
+  
+      // (C4) LIST CURRENT CART ITEMS
+      cart.list();
+    },
+  
+    // (D) LIST CURRENT CART ITEMS (IN HTML)
+    list : function () {
+      // (D1) RESET
+      cart.hItems.innerHTML = "";
+      let item, part, pdt;
+      let empty = true;
+      for (let key in cart.items) {
+        if(cart.items.hasOwnProperty(key)) { empty = false; break; }
+      }
+  
+      // (D2) CART IS EMPTY
+      if (empty) {
+        item = document.createElement("div");
+        item.innerHTML = "Your cart is currently empty...";
+        cart.hItems.appendChild(item);
+      }
+  
+      // (D3) CART IS NOT EMPTY - LIST ITEMS
+      else {
+        let BathP, total = 0, subtotal = 0;
+        for (let id in cart.items) {
+          // ITEM
+          BathP = Bathproducts[id];
+          item = document.createElement("div");
+          item.className = "c-item";
+          cart.hItems.appendChild(item);
+  
+          // NAME
+          part = document.createElement("div");
+          part.innerHTML = BathP.name;
+          part.className = "c-name";
+          item.appendChild(part);
+  
+          // REMOVE
+          part = document.createElement("input");
+          part.type = "button";
+          part.value = "X";
+          part.dataset.id = id;
+          part.className = "c-del cart";
+          part.addEventListener("click", cart.remove);
+          item.appendChild(part);
+  
+          // QUANTITY
+          part = document.createElement("input");
+          part.type = "number";
+          part.value = cart.items[id];
+          part.dataset.id = id;
+          part.className = "c-qty";
+          part.addEventListener("change", cart.change);
+          item.appendChild(part);
+  
+          // SUBTOTAL
+          subtotal = cart.items[id] * BathP.price;
+          total += subtotal;
+        }
+  
+        // EMPTY BUTTONS
+        item = document.createElement("input");
+        item.type = "button";
+        item.value = "Empty";
+        item.addEventListener("click", cart.nuke);
+        item.className = "c-empty cart";
+        cart.hItems.appendChild(item);
+  
+        // CHECKOUT BUTTONS
+        item = document.createElement("input");
+        item.type = "button";
+        item.value = "Checkout - " + "$" + total;
+        item.addEventListener("click", cart.checkout);
+        item.className = "c-checkout cart";
+        cart.hItems.appendChild(item);
+      }
+    },
+  
+    // (E) ADD ITEM INTO CART
+    add : function () {
+      if (cart.items[this.dataset.id] == undefined) {
+        cart.items[this.dataset.id] = 1;
+      } else {
+        cart.items[this.dataset.id]++;
+      }
+      cart.save();
+      cart.list();
+    },
+  
+    // (F) CHANGE QUANTITY
+    change : function () {
+      if (this.value == 0) {
+        delete cart.items[this.dataset.id];
+      } else {
+        cart.items[this.dataset.id] = this.value;
+      }
+      cart.save();
+      cart.list();
+    },
+    
+    // (G) REMOVE ITEM FROM CART
+    remove : function () {
+      delete cart.items[this.dataset.id];
+      cart.save();
+      cart.list();
+    },
+    
+    // (H) CHECKOUT
+    checkout : function () {
+      // SEND DATA TO SERVER
+      // CHECKS
+      // SEND AN EMAIL
+      // RECORD TO DATABASE
+      // PAYMENT
+      // WHATEVER IS REQUIRED
+      alert("TO DO");
+  
+      /*
+      var data = new FormData();
+      data.append('cart', JSON.stringify(cart.items));
+      data.append('products', JSON.stringify(products));
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "SERVER-SCRIPT");
+      xhr.onload = function(){ ... };
+      xhr.send(data);
+      */
+    }
+  };
+  window.addEventListener("DOMContentLoaded", cart.init);
+
+
+
+  
---
+++ product.js
@@ -0,0 +1,293 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var health_beautyproducts = {
+    1: {
+      name : "Organic Body Soap",
+      desc : "made with shea butter and enriched with mango butter rose petal oil, rose hips, French pink clay, and creamy coconut milk.",
+      img : "health1.webp",
+      price : 15.50
+    },
+    2: {
+      name : "eco friendly dish kit",
+      desc : "The perfect cleaning product for your kitchen, it produces a great amount of lather for your dishwashing needs.",
+      img : "health1.webp",
+      price : 30.00
+    },
+    3: {
+      name : "Zero Waste Facial Soap",
+      desc : "This organic body soap will leave your skin feeling nourished and healthy, without stripping it of its natural oils.",
+      img : "health1.webp",
+      price : 10.50
+    },
+    4: {
+      name : "Organic Body Soap",
+      desc : "made with shea butter and enriched with mango butter rose petal oil, rose hips, French pink clay, and creamy coconut milk.",
+      img : "health1.webp",
+      price : 15.50
+    },
+    5: {
+      name : "eco friendly dish kit",
+      desc : "The perfect cleaning product for your kitchen, it produces a great amount of lather for your dishwashing needs.",
+      img : "health1.webp",
+      price : 30.00
+    },
+    6: {
+      name : "Zero Waste Facial Soap",
+      desc : "This organic body soap will leave your skin feeling nourished and healthy, without stripping it of its natural oils.",
+      img : "health1.webp",
+      price : 10.50
+    },
+    7: {
+      name : "Organic Body Soap",
+      desc : "made with shea butter and enriched with mango butter rose petal oil, rose hips, French pink clay, and creamy coconut milk.",
+      img : "health1.webp",
+      price : 15.50
+    },
+    8: {
+      name : "eco friendly dish kit",
+      desc : "The perfect cleaning product for your kitchen, it produces a great amount of lather for your dishwashing needs.",
+      img : "health1.webp",
+      price : 30.00
+    },
+    9: {
+      name : "Zero Waste Facial Soap",
+      desc : "This organic body soap will leave your skin feeling nourished and healthy, without stripping it of its natural oils.",
+      img : "health1.webp",
+      price : 10.50
+    }
+};
+
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var Bathproducts = {
+  1: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  2: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  3: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  4: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  5: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  6: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  7: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  8: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  },
+  9: {
+    name : "Bamboo Toothbrush",
+    desc : "made with bamboo",
+    img : "bath1.jpeg",
+    price : 5.50
+  }
+};
+
+
+
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var Kitchenproducts = {
+  1: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  2: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  3: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  4: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  5: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  6: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  7: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  8: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  },
+  9: {
+    name : "Plastic Free Cutlery",
+    desc : "made with bamboo",
+    img : "kitchen1.jpeg",
+    price : 5.50
+  }
+};
+
+
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var Veganproducts = {
+  1: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  2: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  3: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  4: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  5: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  6: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  7: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  8: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  9: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  }
+};
+
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var zerowasteproducts = {
+  1: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  2: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  3: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  4: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  5: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  6: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  7: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  8: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  },
+  9: {
+    name : "Miyoko Cheese",
+    desc : "made with cashew",
+    img : "vegan2.jpeg",
+    price : 10.00
+  }
+};
+
